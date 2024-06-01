b37c3f8084c6c45b03dafeffe4e77cf0351ca73e
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-06-01 22:12:59 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,12 @@
 get('search').on_submit(function(content)
+  get('title').set_content('Loading')
+  get('content').set_content('Page content loading...')
   local res = fetch({
     url = "https://api.fsh.plus/wikipedia?page="..content,
     method = "GET",
     headers = { ["Content-Type"] = "text/plain" },
     body = 'Text'
   })
+  get('title').set_content(res['title'])
   get('content').set_content(res['data'])
 end)
