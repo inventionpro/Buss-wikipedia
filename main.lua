c65c2bae0e405eb7a9d9a7cc4c5d91b2bd31f70f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-06-01 21:02:32 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,9 @@
-
+get('search').on_submit(function(content)
+  local res = fetch({
+    url = "https://api.wikimedia.org/core/v1/wikipedia/en/page/Earth/html",
+    method = "GET",
+    headers = { ["Content-Type"] = "text/plain" },
+    body = 'Text'
+  })
+  get('content').set_content(res)
+end)
